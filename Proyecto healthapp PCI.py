37718d2149921c3d02f3554599d890373c1d7fa0
AnaMachuca <machucamiranda22@gmail.com>
2024-09-01 21:43:53 -0600

    Add files via upload
    Uso de funciones en cálculo de calorías para hombres y mujeres y para e deficit y superavit calórico

--- Proyecto healthapp PCI.py
+++ Proyecto healthapp PCI.py
@@ -1,5 +1,5 @@
 sexofemenino="Mujer"
-paquete1=("arroz,pollo,frijol,leche,queso,huevo,avena,pasta,lentejas,tortilla,jamón,pan bimbo,crema")
+paquete1=("arroz,pollo,frijol,leche,queso,huevo,avena,pasta,lentejas,tortilla,jamón,pan bimbo")
 paquete2=("arroz,pollo,frijol,leche,queso,yogur,avena,pasta,lentejas,tortilla,jamón,pan bimbo cero, pescado,barra de proteina, rice cakes, huevo,nueces, frutas, hotcakes de avena")
 paquete3=("arroz,pollo,frijol,leche de proteína,queso,yogur griego light,avena,pasta,lentejas,tortilla,jamón,pan oroweat, pescado,barra de proteina, rice cakes, huevo,nueces, frutas, hotcakes de avena, salmón,tocino,carne de res, gelatina light, nopales, canre de cerdo, crema de maní, germen de trigo")
 sexo=str(input("¿Cuál es tu sexo? (Hombre/Mujer): "))
@@ -7,22 +7,35 @@ peso=float(input("¿Cuántos pesas? (en kilos)"))
 edad=int(input("¿Cuál es tu edad?"))
 estatura=float(input("¿Cuál es tu estatura?(en metros)"))
 
+def calorias_de_mantenimientof(peso,estatura,edad):
+   cantidad_de_calorías=(((10*peso)+(6.25*estatura)-(5*edad)-161)*10)
+   return cantidad_de_calorías
 if sexo==sexofemenino:
-    Calorías_de_mantenimiento=(((10*peso)+(6.25*estatura)-(5*edad)-161)*10)
-    print(f"Calorías de mantenimiento {Calorías_de_mantenimiento} calorias")
+    cals=calorias_de_mantenimientof(peso,estatura,edad)
+    print(f"Calorías de mantenimiento {cals} calorias")
 else:
-    Calorías_de_mantenimiento= (((10*peso)+(6.25*estatura)-(5*edad)+5)*10)
-    print(f"Calorías de mantenimiento= {Calorías_de_mantenimiento} calorias")
+ def calorias_de_mantenimientom(peso,estatura,edad):
+  cantidad_de_calorías= (((10*peso)+(6.25*estatura)-(5*edad)+5)*10)
+  return cantidad_de_calorías
+ cals=calorias_de_mantenimientom(peso,estatura,edad)
+ print(f"Calorías de mantenimiento {cals} calorías")
+ 
+def calcular_superavit(cals):
+  superavit=((cals*0.20)+cals)
+  return superavit
+def calcular_deficit(cals):
+  déficit=((cals)-(cals*.20))
+  return déficit
 
 objetivo_fitness=str(input("¿Cuál es tu objetivo fitnes? (Subir de peso,Bajar de peso,Mantenimiento)"))
 if objetivo_fitness=="Subir de peso":
-    superavit=((Calorías_de_mantenimiento*0.20) + (Calorías_de_mantenimiento))
-    print(f"Debes hacer un superavit de: {superavit} calorias")
+   Superavit=calcular_superavit(cals)
+   print(f"Debes hacer un superavit de: {Superavit} calorias")
 elif objetivo_fitness=="Bajar de peso":
-    Déficit=((Calorías_de_mantenimiento)-(Calorías_de_mantenimiento*.20))
+    Déficit=calcular_deficit(cals)
     print(f"Debes hacer un déficit de: {Déficit} calorías")
 else:
-    Mantenimiento=Calorías_de_mantenimiento
+    Mantenimiento=cals
     print("Debes consumir tus calorías de mantenimiento")
 
 presupuesto=str(input("¿Cuál es tu presupuesto semanal? (1000,2000,3000+)"))
@@ -36,7 +49,7 @@ else:
  comida_recomendada=paquete3
 print(f"Opciones saludables de comida para ti:{paquete3}")
 
-print("Lsistos para una vida saludable")
+print("Listos para una vida saludable")
 
  
 
